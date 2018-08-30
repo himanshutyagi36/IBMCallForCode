f71b0a0fa943e3b9ab5d8739c9fc7f8910da2b77
himanshu tyagi <himanshu.tyagi1@ibm.com@himanshus-mbp.raleigh.ibm.com>
2018-08-30 16:51:04 -0400

    #2, code for connecting to cloudant from local and bluemix

--- file-uploader/app.js
+++ file-uploader/app.js
@@ -3,7 +3,38 @@ var app = express();
 var path = require('path');
 var formidable = require('formidable');
 var fs = require('fs');
-var port = 3001
+var cfenv = require("cfenv");
+var Cloudant = require("cloudant");
+var jsonQuery = require('json-query');
+var port = 3001;
+
+var vcapLocal = null;
+try {
+  vcapLocal = require("./vcap-local.json");
+}
+catch (e) {}
+var appEnvOpts = vcapLocal ? {vcap:vcapLocal} : {};
+var appEnv = cfenv.getAppEnv(appEnvOpts);
+// console.log("*****************");
+// console.log(appEnv);
+// console.log("*****************");
+
+// Retrieves service credentials for the input service
+function getServiceCreds(appEnv, serviceName) {
+  var serviceCreds = appEnv.getServiceCreds(serviceName)
+  if (!serviceCreds) {
+    console.log("service " + serviceName + " not bound to this application");
+    return null;
+  }
+  return serviceCreds;
+}
+var cloudantCreds = getServiceCreds(appEnv, "Cloudant-kt"),
+  dbName = "images", 
+  cloudant,
+  db;
+
+// var cloudantCreds = process.env.cloudantCreds;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
@@ -42,14 +73,35 @@ app.post('/upload', function(req, res){
 
 });
 
+function setVcapServices() {
+  var cloudantCredsFromEnv = {};
+	var vcap_services = JSON.parse(process.env.VCAP_SERVICES);	
+	var dbCreds = vcap_services["cloudantNoSQLDB"][0];
+  cloudantCredsFromEnv.username = dbCreds.credentials.username;
+  cloudantCredsFromEnv.password = dbCreds.credentials.password;
+  return cloudantCredsFromEnv;
+}
+
 if (process.env.VCAP_APPLICATION) {
 	//overwrite defaults
 	host = '0.0.0.0';
 	port = process.env.PORT;
 } else {
-	port = 3001;
+  port = 3001;
+  cloudantCreds = setVcapServices();
 }
 
 var server = app.listen(port, function(){
   console.log('Server listening on port '+port);
+  var dbCreated = false;
+  Cloudant({account:cloudantCreds.username, password:cloudantCreds.password}, function(er, dbInstance) { 
+    cloudant = dbInstance;
+    if (er) {
+        return console.log('Error connecting to Cloudant account %s: %s', cloudantCreds.username, er.message);
+    }
+    console.log('Connected to cloudant');
+    cloudant.db.list(function(err, allDbs) {
+      console.log('All my databases: %s', allDbs.join(', '))
+    });
+  });
 });
